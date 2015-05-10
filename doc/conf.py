2c50bfa5f222cfec82147985d5d3055cb0c0863f
Sangeeth <kskrishnasangeeth@gmail.com>
2015-05-10 23:48:33 +0530

    Inits

--- doc/conf.py
+++ doc/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'libcipher'
-copyright = u'2015, Vijay Kumar B.'
+project = u'AlgoPy'
+copyright = u'2015, Krishna Sangeeth KS.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'libcipherdoc'
+htmlhelp_basename = 'algopydoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,8 +196,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'libcipher.tex', u'libcipher Documentation',
-   u'Vijay Kumar B.', 'manual'),
+  ('index', 'algopy.tex', u'AlgoPy Documentation',
+   u'Krishna Sangeeth KS', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +226,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'libcipher', u'libcipher Documentation',
-     [u'Vijay Kumar B.'], 1)
+    ('index', 'AlgoPy', u'AlgoPy Documentation',
+     [u'Krishna Sangeeth KS.'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'libcipher', u'libcipher Documentation',
-   u'Vijay Kumar B.', 'libcipher', 'One line description of project.',
+  ('index', 'algopy', u'AlgoPy Documentation',
+   u'Krishna Sangeeth KS', 'AlgoPy', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -258,4 +258,4 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 # releases plugin settings
-releases_github_path = 'chennaipy/libcipher'
+releases_github_path = 'kskrishnasangeeth/AlgoPy'
